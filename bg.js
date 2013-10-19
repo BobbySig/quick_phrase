c3e44c1f516a150e96d6b9270116e945556f3446
Bobby Signor <signor.bobby@gmail.com>
2013-10-19 00:14:08 -0600

    Implement text copying.

--- bg.js
+++ bg.js
@@ -1,5 +1,22 @@
 {
+	//Creates the panel when browserAction is clicked.
 	chrome.browserAction.onClicked.addListener(function() {
         	chrome.windows.create({'url':'thing.html', 'type': 'panel'}, function(window) { });
     	});
+
+	//Executes clipText when message is sent from content scripts.
+	chrome.runtime.onMessage.addListener(
+		function(msg, sender, sendResponse) {
+        	clipText(msg.clip);
+	});
+
+	//Copies passed in String to the clipboard.
+	function clipText(s) {
+		console.log("In clipText method.");
+		area = document.getElementById('clip-temp');
+		area.value = s;
+		area.focus();
+		area.select();
+		document.execCommand('copy', false, null);
+	}
 }
