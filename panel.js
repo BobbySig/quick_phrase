7de9e884787b3598ca0769c607a215b5f1026298
Bobby Signor <signor.bobby@gmail.com>
2015-05-31 21:37:43 -0800

    BAD

--- panel.js
+++ panel.js
@@ -5,13 +5,19 @@
 	function listFormatter(items) {
 		var htmlString = "";
 
+		htmlString += "<div style='max-height: " + window.innerHeight + ";'>"
+
 		//Generates HTML for each phrase.
 		for (i = 0; i < items.length; i++) {
-			htmlString += "<a id='phrase' href='#'><div>" + items[i] + "</div></a><hr>";
+			htmlString += "<a id='phrase' href='#'>";
+			htmlString += "<div>";
+			htmlString += items[i] + "</div></a><hr>";
 		}
 
+		htmlString += "</div>";
+
 		//Generates HTML for "Create New Phrase" button.
-		htmlString += "<form id='create'><input id='new-phrase'><button type='button' id='create-new'>Add</button></form>";
+		//htmlString += "<form id='create'><input id='new-phrase'><button type='button' id='create-new'>Add</button></form>";
 
 		return htmlString;
 	}
@@ -20,37 +26,14 @@
 		items.push(s);
 		return items;
 	}
-	
-	//Displays the list of phrases.
-	function displayPhraseList() {
-		document.getElementById('items').innerHTML = listFormatter(things);
 
-		//Perform action on all links in document.
-		var allPhrases = document.links;
-		for (i = 0; i < allPhrases.length; i++) {
-			//Sends clipped text to background scripts for copying on click.
-			if (allPhrases[i].id = "phrase") {
-				allPhrases[i].onclick = function() {
-					var clipText = this.innerText;
-					chrome.runtime.sendMessage({type: "action-copy", clip: clipText});
-				}
-			}
-		}
-
-		document.forms['create'].onsubmit = function() {
-			document.forms['create']['create-new'].click();
-			return false;
-		}
-
-		//Set action for 'create-new' button.
-		document.forms['create']['create-new'].onclick = function() {
-			var newPhrase = document.forms['create']['new-phrase'].value;
-			if (newPhrase != "" && newPhrase != null) {
-				things = addToList(things, newPhrase);
-				document.forms['create']['new-phrase'].value = "";
-				displayPhraseList();
-			}
-		}
+	function showRClickMenu(control, e) {
+		var mouseX = e.clientX + window.pageXOffset + 'px'; //Mouse X.
+		var mouseY = e.clientY + window.pageYOffset + 'px'; //Mouse Y.
+		document.getElementById(control).style.position = 'absolute';
+		document.getElementById(control).style.display = 'inline';
+		document.getElementById(control).style.left = posx;
+		document.getElementById(control).style.top = posy;
 	}
 
 	//Actions done after page is loaded.
@@ -65,6 +48,16 @@
 				allPhrases[i].onclick = function() {
 					var clipText = this.innerText;
 					chrome.runtime.sendMessage({type: "action-copy", clip: clipText});
+					return false;
+				}
+				
+				allPhrases[i].oncontextmenu = function() {
+					var mouseX = window.event.clientX + window.pageXOffset + 'px'; //Mouse X.
+					var mouseY = window.event.clientY + window.pageYOffset + 'px'; //Mouse Y.
+					document.getElementById('contextMenu').style.position = 'absolute';
+					document.getElementById('contextMenu').style.display = 'inline';
+					document.getElementById('contextMenu').style.left = mouseX;
+					document.getElementById('contextMenu').style.top = mouseY;
 				}
 			}
 		}
@@ -80,8 +73,20 @@
 			if (newPhrase != "" && newPhrase != null) {
 				things = addToList(things, newPhrase);
 				document.forms['create']['new-phrase'].value = "";
-				displayPhraseList();
+				window.onload();
 			}
+		}
+
+		document.body.oncontextmenu = function() {
+			return false;
+		}
+
+		document.onmousedown = function() {
+			document.getElementById('contextMenu').style.display = 'none';
+		}
+
+		document.onmouseup = function() {
+			document.getElementById('contextMenu').style.display = 'none';
 		}
 	}
 }
